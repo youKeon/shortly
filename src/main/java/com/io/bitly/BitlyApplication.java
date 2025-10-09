63c5935fd183d9c497fc6df332b6b6e5dac4245f
유건 <k.yu@okestro.com>
2025-10-09 20:49:22 +0900

    init

---
+++ src/main/java/com/io/bitly/BitlyApplication.java
@@ -0,0 +1,13 @@
+package com.io.bitly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/io/bitly/BitlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.bitly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
