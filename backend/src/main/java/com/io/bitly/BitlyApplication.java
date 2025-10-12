e4dbcc3e9b0bc5ad34a5ae0c98308e13121f1dbe
유건 <k.yu@okestro.com>
2025-10-12 19:40:08 +0900

    test: phase 3 구현

--- backend/src/main/java/com/io/bitly/BitlyApplication.java
+++ backend/src/main/java/com/io/bitly/BitlyApplication.java
@@ -2,9 +2,9 @@ package com.io.bitly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableAsync
+@EnableScheduling
 @SpringBootApplication
 public class BitlyApplication {
 
