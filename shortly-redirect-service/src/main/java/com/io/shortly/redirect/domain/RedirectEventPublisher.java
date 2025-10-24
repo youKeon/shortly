e90034f95f2204831d920b201bd7284622014119
유건 <k.yu@okestro.com>
2025-10-24 10:39:40 +0900

    feat: 리다이렉트 서비스 추가

---
+++ shortly-redirect-service/src/main/java/com/io/shortly/redirect/domain/RedirectEventPublisher.java
@@ -0,0 +1,8 @@
+package com.io.shortly.redirect.domain;
+
+import com.io.shortly.shared.event.UrlClickedEvent;
+
+public interface RedirectEventPublisher {
+
+    void publishUrlClicked(UrlClickedEvent event);
+}
